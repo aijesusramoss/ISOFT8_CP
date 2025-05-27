f47d74caa2a8c7b5b371fa38b3a81b4b7125d9b6
AngelCarrillo00 <aroncarrillo05@gmail.com>
2025-05-27 13:53:07 -0700

    primer commit

---
+++ Drunkard.py
@@ -0,0 +1,42 @@
+import time
+import random
+import os
+
+def limpiar_pantalla():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+while True:
+
+    nombre_estado = {"Carlitos": 0, "Antuna": 0, "Zuñiga": 0, "Cesar": 0, "Ramiro": 0}
+    valores = random.sample(range(1, 6), len(nombre_estado))  
+    nombre_estado = dict(zip(nombre_estado.keys(), valores))  
+
+    def tomar(nombre):
+        if nombre_estado[nombre] == 1:
+            print(f"{nombre} tomando cerveza...")
+            time.sleep(1)
+
+    def usar_baño(nombre):
+        if nombre_estado[nombre] == 2:
+            print(f"{nombre} orinando...")
+            time.sleep(1)
+            print(f"{nombre} salió del baño...")
+
+    def llamar_ex(nombre):
+        if nombre_estado[nombre] == 3:
+            print(f"{nombre} llamando a su ex...")
+
+    def cantar(nombre):
+        if nombre_estado[nombre] == 4:
+            print(f"{nombre} está cantando...")
+
+    acciones = [tomar, usar_baño, llamar_ex, cantar]
+
+    def main():
+        for accion in acciones:
+            for nombre in nombre_estado:
+                accion(nombre)
+    print(f'================================Termino El Ciclo ========================================')
+
+
+    main()
